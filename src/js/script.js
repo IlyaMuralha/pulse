41a4ac6454e1f1832c8ec7697dcb070da60a94d0
IlyaMuralha <muralhakuzmenko@gmail.com>
2022-10-30 22:48:13 +0500

    create tabs

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,5 @@
-// слайдер с помощью слик
-// $(document).ready(function(){
+$(document).ready(function () {
+    // слайдер с помощью слик
 //     $('.carousel__inner').slick({
 //         speed: 1000,
 //         adptiveHeight: false,
@@ -17,21 +17,51 @@
 //             }
 //         ]
 //     });
-//   });
+
+    const slider = tns({
+        container: '.carousel__inner',
+        items: 1,
+        slideBy: 'page',
+        autoplay: false,
+        controls: false,
+        nav: false
+    });
+    
+    document.querySelector('.prev').addEventListener('click', function () {
+        slider.goTo('prev');
+    });
+    document.querySelector('.next').addEventListener('click', function () {
+        slider.goTo('next');
+    });
+
+    $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+        $(this)
+          .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+            .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($
+                (this).index()).addClass('catalog__content_active');
+    });
+
+    function toggleSlide(item) {
+        $(item).each(function (i) {
+            $(this).on('click', function (e) {
+                e.preventDefault();
+                $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+                $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+            });
+        });
+    }
+
+    toggleSlide('.catalog-item__link');
+    toggleSlide('.catalog-item__back');
+    
+    // $('.catalog-item__link').each(function (i) {
+    //     $(this).on('click', function (e) {
+    //         e.preventDefault();
+    //         $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+    //         $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+    //     });
+    // });
+
+});
 
 
-const slider = tns({
-    container: '.carousel__inner',
-    items: 1,
-    slideBy: 'page',
-    autoplay: false,
-    controls: false,
-    nav: false
-});
-  
-document.querySelector('.prev').addEventListener('click', function () {
-    slider.goTo('prev');
-});
-document.querySelector('.next').addEventListener('click', function () {
-    slider.goTo('next');
-});
