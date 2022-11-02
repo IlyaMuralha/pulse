f02391198904010a08b038acceb0f337c2b5916a
IlyaMuralha <muralhakuzmenko@gmail.com>
2022-11-02 13:20:22 +0500

    added animate

--- src/js/script.js
+++ src/js/script.js
@@ -129,6 +129,24 @@ $(document).ready(function () {
         return false;
     });
 
+    // smooth scroll
+
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 1600) {
+            $('.pageup').fadeIn();
+        } else {
+            $('.pageup').fadeOut();
+        }
+    });
+
+    $("a[href='#up']").click(function(){
+        const _href = $(this).attr("href");
+        $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+        return false;
+    });
+
+    new WOW().init();
+
 });
 
 
